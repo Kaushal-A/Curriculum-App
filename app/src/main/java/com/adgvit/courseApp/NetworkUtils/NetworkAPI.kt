7561723ae8b87d2513bb6eb6eb5a1de7eb960416
raehat <raehatsinghnanda@gmail.com>
2022-01-29 00:54:21 +0530

    lmaoo

--- app/src/main/java/com/adgvit/courseApp/NetworkUtils/NetworkAPI.kt
+++ app/src/main/java/com/adgvit/courseApp/NetworkUtils/NetworkAPI.kt
@@ -2,15 +2,18 @@ package com.adgvit.courseApp.NetworkUtils
 
 import android.telecom.Call
 import com.adgvit.courseApp.Models.AllCourses
+import com.adgvit.courseApp.Models.Course
 import org.json.JSONObject
-import retrofit2.http.GET
-import retrofit2.http.Header
-import retrofit2.http.Headers
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface NetworkAPI {
 
     @Headers("Authorization: E5sRRXq3mS2BoUJ")
     @GET("curriculum")
     fun getAllCurriculums(@Query("page") pageNo: String): retrofit2.Call<AllCourses>
+
+    @Headers("Authorization: E5sRRXq3mS2BoUJ")
+    @GET("curriculum/code/{code}")
+    fun getCurriculumFromCode(@Path("code") code: String): retrofit2.Call<Course>
+
 }
--- app/src/main/java/com/adgvit/courseApp/activity/CourseActivity.kt
+++ app/src/main/java/com/adgvit/courseApp/activity/CourseActivity.kt
@@ -2,20 +2,31 @@ package com.adgvit.courseApp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.adgvit.courseApp.Models.Course
+import com.adgvit.courseApp.NetworkUtils.NetworkUtils
 import com.adgvit.courseApp.R
 import com.adgvit.courseApp.databinding.ActivityCourseBinding
 import com.adgvit.courseApp.databinding.ActivityHomeBinding
 import com.google.android.material.tabs.TabLayout
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class CourseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityCourseBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(R.layout.activity_course)
 
+        var call: Call<Course> = NetworkUtils().getNetworkAPIInstance().getCurriculumFromCode("CSE2013")
 
+        GlobalScope.launch {
+            Log.i("MADARCHOD", call.execute().body()?.code.toString())
+        }
 
     }
 }
