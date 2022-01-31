30729f0c32bfb0eebafda363f97418a04b58746c
JainKhushi15 <khushi.jain2020@vitstudent.ac.in>
2022-01-31 08:44:07 +0530

    Intent Activity 4

--- app/src/main/java/com/adgvit/courseApp/activity/Module.kt
+++ app/src/main/java/com/adgvit/courseApp/activity/Module.kt
@@ -1,19 +1,13 @@
 package com.adgvit.courseApp.activity
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.ListView
 import android.widget.TextView
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.adgvit.courseApp.R
 import com.adgvit.courseApp.rvAdapters.ModuleRVAdapter
+import java.util.ArrayList
 
 class Module : AppCompatActivity() {
     private var layoutManager: RecyclerView.LayoutManager? = null
@@ -23,12 +17,10 @@ class Module : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_module)
 
-        //val titles = intent.getSerializableExtra("keyName")
-
-        val titles = ArrayList<String>()
+        val titles: ArrayList<String> = intent.getStringArrayListExtra("modData") as ArrayList<String>
+        /*titles.add("Mod 1")
         titles.add("Mod 1")
-        titles.add("Mod 1")
-        titles.add("Mod 1")
+        titles.add("Mod 1")*/
 
         val ModuleRV = findViewById<RecyclerView>(R.id.MRV)
         val MName = findViewById<TextView>(R.id.ModuleName)
